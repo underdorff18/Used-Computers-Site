c362ddbb1e629d0f0520b6864b7341e7d616318a
Nick Overdorff <nickoverdorff@yahoo.com>
2020-10-08 21:03:53 -0400

    Added json parser

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,10 @@
-const express = require('express' 4.17.1');
+const express = require('express', '4.17.1');
 const app = express();
+const myparse = require('./parse');
 const port = 3000;
 
 
+console.log(JSON.stringify(myparse.systems));
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
